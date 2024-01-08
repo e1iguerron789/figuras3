e8d697e496f115ddc4c083abb019dae4af700dd2
e1iguerron789 <130955581+e1iguerron789@users.noreply.github.com>
2024-01-07 21:52:34 -0500

    Update FInterfaz.java

---
+++ FInterfaz.java
@@ -0,0 +1,7 @@
+package EjercicioFigura;
+
+public interface FInterfaz {
+    public boolean isGreater(Object a, Object b);
+    public boolean isLess(Object a, Object b);
+    public boolean isEqual(Object a, Object b);
+}
